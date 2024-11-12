9c469d18ac27c9849b5f9d9b1e757ee1ed44e09f
Zheng Qiao <109362533+BossJoeZz@users.noreply.github.com>
2024-11-12 13:36:51 -0800

    Database.py

---
+++ app/database.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+db_config = {
+    'drivername': 'mysql+pymysql',
+    'host': 'localhost',
+    'port': 3306,
+    'username': 'root',
+    'password': 'qz325299',
+    'database': 'common_assess'
+}
+
+engine = create_engine(f"{db_config['drivername']}://{db_config['username']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}")
+
+# Create a base class to define the database model
+Base = declarative_base()
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
